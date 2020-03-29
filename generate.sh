9e2932fd3eb2e2f695eb2311e6114113005db307
Senor Rossie <senorrossie@users.noreply.github.com>
2020-03-29 22:31:09 +0200

    Create valid atr's, with mypicodos et. al

--- generate.sh
+++ generate.sh
@@ -4,78 +4,132 @@
 #set -xv
 
 ### Settings
-source config
+source config 2>/dev/null
 
-let MAXSIZE=16360
+# Defaults if unset
+FANDAL=${FANDAL:-"work/a8_fandal_cz"}
+ATRDIR=${ATRDIR:-"work/16M/"}
+MYPICO=${NYPICO:-"MyPicoDos406S1"}
+let MAXSIZE=${MAXSIZE:-16360}
+let FMAX=${FMAX:-62}
 
+# Counters
+let TD=0            # Total disks
+let TF=0            # Total files
+let TF=0            # Total size
+
+# Store IFS
+OLDIFS=$IFS
+
+# Sanity check
 if [ '$( ls -1 "${FANDAL}*" )' == '' ]; then
     printf "Unable to locate the fandal archive in %s*!" "${FANDAL}"
     printf "You can manually download and extract it (an archiver that works is 7zip) to the work directory.\n"
     exit 1
 fi
+if [ ! -e "${ATRDIR}" ]; then 
+    printf "Destination directory %s does not exist. Please create it first!\n" "${ATRDIR}"
+    exit 1
+fi
 
 function prep_disks() {
-    let TD=0            # Total disks
-    let TF=0            # Total files
-    let TF=0            # Total size
+    let C=1             # DISK
+    let N=0             # Number of files
+    let D=0             # Directory #
+    let DE=0            # Directory Entry
     let SIZE=0
 
-    OLDIFS=$IFS
-    for TYPE in GAME DEMO; do
-        printf "Processing Fandal's %s collection in %s* (Destination: %s)...\n" "${TYPE}" "${FANDAL}" "$ATRDIR"
+    OLDDIR=""
+    OUTDIR="${ATRDIR}${TYPE}"
+    PREVIOUS=""
+    XTRA=""
 
-        # Create base destination directory
-        OUTDIR="${ATRDIR}${TYPE}"
-        if [ ! -d "${OUTDIR}" ]; then
-            mkdir -p "${ATRDIR}"
+    IFS=$'\n'
+    for BIN in `find ${SDIR} -type f -name "*.xex" | sort -n`; do
+        let N++
+        let DE++
+
+        DST=`echo $BIN | rev | cut -d / -f 1 | rev`
+        SUBDIR=`echo $BIN | rev | cut -d / -f 2 | rev`
+        if [ x$SUBDIR != x$OLDDIR ]; then
+            # New letter
+            OLDDIR=${SUBDIR}
+            let D=0     # Directory #
+            let DE=1    # Directory entry
+            XTRA=""
         fi
 
-        let C=1             # DISK
-        let N=0             # Number of files
-        let SIZE=0
-
-        IFS=$'\n'
-        for BIN in `find ${FANDAL}*/Binaries/Games/ -type f -name "*.xex"`; do
-            let N++
-            FSIZE=$(du "${BIN}" | awk '{print $1}')
-            let SIZE=$(($SIZE + $FSIZE))
-            if [ $SIZE -ge $MAXSIZE ]; then
-                let DISKSIZE=$(($SIZE-$FSIZE))
-                let TS=$((TS + DISKSIZE))
-                printf " - Disk: %s\t(Size: %s, Files: %s)\n" "$C" "$DISKSIZE" "$N"
-                let C++
-                let TD=$((TD + 1))
-                let SIZE=${FSIZE}
-                let TF=$(($TF + $N - 1))
-                let N=1
+        if [ $DE -eq $FMAX ]; then
+            let D++
+            XTRA="${DST:1:1}"
+            XTRA=${XTRA^^}
+            XTRA=${XTRA// /_}
+            if [ -e "$OUTDIR$C/$SUBDIR$XTRA" ]; then
+                XTRA="${DST:1:1}$D"
+                XTRA=${XTRA^^}
+                XTRA=${XTRA// /_}
             fi
-            DST=`echo $BIN | rev | cut -d / -f 1,2 | rev`
-            SUBDIR=`echo $DST | cut -d / -f 1`
-            if [ ! -d "${OUTDIR}${C}/${SUBDIR}" ]; then
-                mkdir -p ${OUTDIR}${C}/${SUBDIR}
-            fi
+            let DE=1
+        fi
 
-            cp -a "$BIN" "$OUTDIR$C/$DST"
-        done
-        let TS=$((TS + SIZE))
-        let TD=$((TD + 1))
-        let TF=$(($TF + $N))
-        printf " - Disk: %s\t(Size: %s, Files: %s)\n" "$C" "$SIZE" "$N"
+        FSIZE=$(du "${BIN}" | awk '{print $1}')
+        let SIZE=$(($SIZE + $FSIZE))
+        if [ $SIZE -ge $MAXSIZE ]; then
+            let DISKSIZE=$(($SIZE-$FSIZE))
+            let TS=$((TS + DISKSIZE))
+            printf " - Disk: %s\t(Size: %s, Files: %s)\n" "$C" "$DISKSIZE" "$N"
+            let C++
+            let TD=$((TD + 1))
+            let SIZE=${FSIZE}
+            let TF=$(($TF + $N - 1))
+            let N=1
+            let D=0
+            let DE=0
+        fi
+
+        if [ ! -d "$OUTDIR$C/$SUBDIR$XTRA" ]; then
+            mkdir -p $OUTDIR$C/$SUBDIR$XTRA
+        fi
+
+        #echo $DE\) \"$BIN\" \"$OUTDIR$C/$SUBDIR$XTRA/$DST\"
+        if [ ! -e "$OUTDIR$C/$SUBDIR$XTRA/$DST" ]; then
+            cp -a "$BIN" "$OUTDIR$C/$SUBDIR$XTRA/$DST"
+        fi
     done
-    IFS=$OLDIFS
-
-    printf "\nCreated %s atr image directories, with %s files. Total size: %s\n" "$TD" "$TF" "$TS"
+    let TS=$((TS + SIZE))
+    let TD=$((TD + 1))
+    let TF=$(($TF + $N))
+    printf " - Disk: %s\t(Size: %s, Files: %s)\n" "$C" "$SIZE" "$N"
 }
 
 function gen_disks() {
-    for DISK in `ls -1 ${ATRDIR}`; do
-        printf "Creating atr for %s\n" "$DISK"
+    for DISK in `ls -1 ${ATRDIR} | grep -v atr$`; do
+        printf " -> Creating atr for %s\n" "$DISK"
         if [ ! -x tools/dir2atr ]; then
             printf "Unable to execute dir2atr from the tools directory.\n"
+            break
         fi
-        tools/dir2atr -d -b ${MYPICO} -P 65535 ${ATRDIR}${DISK}.atr ${ATRDIR}${DISK}
+        WORKDIR=`pwd`
+        TOOLDIR=`dirname ${WORKDIR}/tools/dir2atr`
+        cd ${ATRDIR}${DISK}
+        ${TOOLDIR}/dir2atr -d -b ${MYPICO} -P 65535 ../${DISK}.atr .
+        cd -
     done
 }
 
-#prep_disks
+### Main
+for TYPE in GAME DEMO; do
+    case $TYPE in
+        GAME)
+            SDIR="${FANDAL}*/Binaries/Games"
+            ;;
+        DEMO)
+            SDIR="${FANDAL}*/Binaries/Demos"
+            ;;
+    esac
+    printf "Processing Fandal's %s collection in %s* (Destination: %s)...\n" "${TYPE}" "${SDIR}" "$ATRDIR"
+    prep_disks
+    printf "\nCreated %s atr image directories, with %s files. Total size: %s\n\n" "$TD" "$TF" "$TS"
+done
+
 gen_disks
